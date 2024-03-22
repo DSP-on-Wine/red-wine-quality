738cf04b3e1afec87d9b9a59a12da2f1a642ab77
Beemnet <bemnetteklu@gmail.com>
2024-03-22 22:34:48 +0100

    code refactoring and changing metric used

--- wine_quality/train.py
+++ wine_quality/train.py
@@ -1,10 +1,11 @@
 import pandas as pd
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_log_error
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from .preprocess import preprocess_data
 import numpy as np
 import joblib
+from . import FEATURE_COLS, TARGET_COL, MODEL_PATH
 
 
 def compute_rmse(
@@ -12,13 +13,12 @@ def compute_rmse(
     y_pred: np.ndarray,
     precision: int = 2
 ) -> float:
-    rmse = np.sqrt(mean_squared_error(y_true, y_pred))
-    return round(rmse, precision)
+    rmsle = np.sqrt(mean_squared_log_error(y_true, y_pred))
+    return round(rmsle, precision)
 
 
 def build_model(data: pd.DataFrame) -> dict:
-    X = data.drop('quality', axis=1)
-    y = data['quality']
+    X, y = data[FEATURE_COLS], data[TARGET_COL]
 
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2,
                                                       random_state=42)
@@ -28,8 +28,8 @@ def build_model(data: pd.DataFrame) -> dict:
 
     model = RandomForestRegressor(random_state=42)
     model.fit(X_train_processed, y_train)
-    joblib.dump(model, '../models/model.joblib')
+    joblib.dump(model, MODEL_PATH)
 
     predictions = model.predict(X_val_processed)
 
-    return {'rmse': compute_rmse(y_val, predictions)}
+    return {'rmlse': compute_rmse(y_val, predictions)}
