9be4fd9a081eb63723be6921f5c067fcac396aaf
Beemnet <bemnetteklu@gmail.com>
2024-03-22 15:53:39 +0100

    code refactoring and changing metric used

--- wine_quality/preprocess.py
+++ wine_quality/preprocess.py
@@ -2,29 +2,16 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 import joblib
-
-NUMERICAL_FEATURES = ['fixed acidity', 'volatile acidity', 'citric acid',
-                      'residual sugar', 'chlorides', 'free sulfur dioxide',
-                      'total sulfur dioxide', 'density', 'pH', 'sulphates',
-                      'alcohol']
+from . import NUMERICAL_FEATURES, SCALER_PATH
 
 
 def preprocess_data(data: pd.DataFrame, is_train: bool) -> np.ndarray:
     scaler = StandardScaler()
     if is_train:
         scaler.fit(data[NUMERICAL_FEATURES])
-        dump_scaler(scaler, '../models/')
+        joblib.dump(scaler, SCALER_PATH)
     else:
-        scaler = load_scaler('../models/')
+        scaler = joblib.load(SCALER_PATH)
 
     X_scaled = scaler.transform(data[NUMERICAL_FEATURES])
     return X_scaled
-
-
-def dump_scaler(scaler, model_path: str) -> None:
-    joblib.dump(scaler, f'{model_path}/scaler.joblib')
-
-
-def load_scaler(model_path: str):
-    scaler = joblib.load(f'{model_path}/scaler.joblib')
-    return scaler
