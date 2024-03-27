89b62fa42a5de0a4cce3369297990bcbff5b077a
Beemnet <bemnetteklu@gmail.com>
2024-03-27 11:10:50 +0100

    completed batch predictions

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -3,6 +3,7 @@ from .models import InputData, Prediction
 from .preprocessing import preprocess_data
 from . import MODEL_PATH, SCALER_PATH
 import joblib
+from typing import List
 
 
 app = FastAPI()
@@ -17,3 +18,12 @@ def predict(data: InputData):
     prediction = model.predict(processed_data)
     prediction_response = Prediction(prediction=prediction[0])
     return prediction_response
+
+@app.post("/predict_batch/", response_model=List[Prediction])
+async def predict_batch(data: List[InputData]):
+    predictions = []
+    for input_data in data:
+        processed_data = preprocess_data(input_data, scaler)
+        prediction = model.predict(processed_data)
+        predictions.append(Prediction(prediction=prediction))
+    return predictions
