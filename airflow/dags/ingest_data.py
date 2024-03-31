9921909df2abbff488062f71b28ad516e524e539
Beemnet <bemnetteklu@gmail.com>
2024-04-01 00:58:05 +0200

    data ingestion with two tasks for submission

--- airflow/dags/ingest_data.py
+++ airflow/dags/ingest_data.py
@@ -1,46 +1,51 @@
 import logging
-import pandas as pd
+import os
+import random
 from datetime import datetime, timedelta
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
-ROWS_PER_FILE = 10
-
+# Define the absolute paths to raw_data and good_data directories
+RAW_DATA_DIR = '/sources/raw_data'
+GOOD_DATA_DIR = 'good_data'
 
 @dag(
     dag_id='ingest_wine_data',
-    description='Ingest data from winequality-red.csv into multiple files',
-    tags=['dsp', 'data_ingestion'],
+    description='Move a randomly selected file from raw-data to good-data',
+    tags=['data_movement', 'dsp', 'data_ingestion'],
     schedule_interval=timedelta(days=1),
-    start_date=days_ago(n=1)
+    start_date=days_ago(n=1)    
 )
 def ingest_wine_data():
 
     @task
-    def split_and_ingest_data() -> None:
-        # Read the original CSV file -- replace with input data later
-        filepath = '../../data/winequality-red.csv'
-        input_data_df = pd.read_csv(filepath)
-
-        # Split the data into chunks of 10 rows each
-        num_chunks = len(input_data_df) // ROWS_PER_FILE
-        data_chunks = [input_data_df.iloc[
-            i:i+ROWS_PER_FILE] 
-            for i in range(0, len(input_data_df), ROWS_PER_FILE)]
-
-        # Ingest each chunk of data
-        for i, chunk in enumerate(data_chunks):
-            save_data(chunk, i)
+    def read_data() -> str:
+        # List all files in the raw data directory
+        raw_files = os.listdir(RAW_DATA_DIR)
+        
+        if raw_files:
+            # Select a random file from the list
+            random_file = random.choice(raw_files)
+            file_path = os.path.join(RAW_DATA_DIR, random_file)
+            logging.info(f"Selected file {random_file} from raw-data.")
+            return file_path
+        else:
+            logging.info("No files found in raw-data directory.")
+            return None
 
     @task
-    def save_data(chunk: pd.DataFrame, chunk_id: int) -> None:
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filepath = f'../output_data/chunk_{chunk_id}_{timestamp}.csv'
+    def save_file(file_path: str) -> None:
+        if file_path:
+            # Move the selected file to the good data directory
+            file_name = os.path.basename(file_path)
+            new_file_path = os.path.join(GOOD_DATA_DIR, file_name)
+            os.rename(file_path, new_file_path)
+            logging.info(f"Moved file {file_name} from raw-data to good-data.")
+        else:
+            logging.info("No file to save.")
 
-        logging.info(f'Ingesting data chunk {chunk_id} to the file: {filepath}')
-        chunk.to_csv(filepath, index=False)
-
-    split_and_ingest_data()
-
+    # Define the task dependency
+    file_path = read_data()
+    save_file(file_path)
 
 ingest_wine_data_dag = ingest_wine_data()
